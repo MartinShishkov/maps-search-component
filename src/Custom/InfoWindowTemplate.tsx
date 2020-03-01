6b10eb1027402be8f3072c82d9af3e24ba0b8a4d
MartinShishkov <martin@codeinsiders.net>
2020-03-01 10:01:50 +0200

    info window

--- src/Custom/InfoWindowTemplate.tsx
+++ src/Custom/InfoWindowTemplate.tsx
@@ -4,13 +4,9 @@ import ISimpleData from './ISimpleData';
 export const InfoWindowTemplate: React.FC<ISimpleData> = (props) =>  
     <div>
         <h2 style={{margin: 0}}>{props.title}</h2>
-        {props.available 
-        ? (
-            <h3 style={{color: "green", margin: 0}}>AVAILABLE</h3>
-        )
-        : (
-            <h3 style={{color: "red", margin: 0}}>NOT AVAILABLE</h3>
-        )}
+        <h3 style={{color: props.available ? "green" : "red", margin: 0}}>
+            {props.available ? "AVAILABLE" : "NOT AVAILABLE"}
+        </h3>
         <table>
             <tr>
                 <td style={{width: "200px"}}>
